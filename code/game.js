c78a486fdae84fab7441fbad5f96d6dde7fec57f
Henry-H08 <143623813+Henry-H08@users.noreply.github.com>
2023-10-16 09:44:34 -0400

    Update game.js

--- code/game.js
+++ code/game.js
@@ -1,17 +1,76 @@
-scene('game', () => {
-  const apple = add([
-    sprite('apple'),
-    pos(SCALE*3, SCALE*3),
-    scale((SCALE/64) * 3), // SCALE divided by sprite width, multiplied by the size change
-    rotate(0),
-    anchor('center'),
 
-  ]);
+loadSprite("apple", "sprites/apple.png")
+loadSound("score", "sounds/pop.mp3")
 
-  onUpdate(() => {
-    apple.angle += dt()*150;
-    apple.pos = mousePos();
-  });
-});
+const SPEED = 480
 
-go('game');
+setGravity(2400)
+
+const level = addLevel([
+	// Design the level layout with symbols
+	"@  ^ $$",
+	"=======",
+], {
+	// The size of each grid
+	tileWidth: 64,
+	tileHeight: 64,
+	// The position of the top left block
+	pos: vec2(100, 200),
+	// Define what each symbol means (in components)
+	tiles: {
+		"@": () => [
+			sprite("apples"),
+			area(),
+			body(),
+			anchor("bot"),
+			"player",
+		],
+		"=": () => [
+			sprite("apples"),
+			area(),
+			body({ isStatic: true }),
+			anchor("bot"),
+		],
+		"$": () => [
+			sprite("apples"),
+			area(),
+			anchor("bot"),
+			"coin",
+		],
+		"^": () => [
+			sprite("apples"),
+			area(),
+			anchor("bot"),
+			"danger",
+		],
+	},
+})
+
+// Get the player object from tag
+const player = level.get("player")[0]
+
+// Movements
+onKeyPress("space", () => {
+	if (player.isGrounded()) {
+		player.jump()
+	}
+})
+
+onKeyDown("left", () => {
+	player.move(-SPEED, 0)
+})
+
+onKeyDown("right", () => {
+	player.move(SPEED, 0)
+})
+
+// Back to the original position if hit a "danger" item
+player.onCollide("danger", () => {
+	player.pos = level.tile2Pos(0, 0)
+})
+
+// Eat the coin!
+player.onCollide("coin", (coin) => {
+	destroy(coin)
+	play("score")
+})
